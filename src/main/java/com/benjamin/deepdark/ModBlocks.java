cc4a4f3113055d3482e350103a3664384f7c76e4
Ruben Neuteboom <ruben.neuteboom@gmail.com>
2026-02-15 10:57:52 +0100

    🔧 Fix Material API for 1.20.1
    - Removed deprecated Material import
    - Use AbstractBlock.Settings.create() instead
    - Material class doesn't exist in 1.20.1!

--- src/main/java/com/benjamin/deepdark/ModBlocks.java
+++ src/main/java/com/benjamin/deepdark/ModBlocks.java
@@ -2,9 +2,9 @@ package com.benjamin.deepdark;
 
 import com.benjamin.deepdark.block.DeepDarkPortalBlock;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.MapColor;
-import net.minecraft.block.Material;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -13,7 +13,8 @@ import net.minecraft.util.Identifier;
 public class ModBlocks {
 	// Deep Dark Portal block
 	public static final Block DEEP_DARK_PORTAL = registerBlock("deep_dark_portal",
-		new DeepDarkPortalBlock(FabricBlockSettings.of(Material.PORTAL, MapColor.CYAN)
+		new DeepDarkPortalBlock(AbstractBlock.Settings.create()
+			.mapColor(MapColor.CYAN)
 			.noCollision()
 			.strength(-1.0F)
 			.sounds(BlockSoundGroup.GLASS)
